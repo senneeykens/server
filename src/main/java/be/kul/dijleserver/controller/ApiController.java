0340ccfb0b7ffd8a9a799557b65c6e174f32fa22
Koen Dehaen <koen.dehaen@roots.be>
2018-01-27 18:24:20 +0100

    integrate pod selection in menu

--- src/main/java/be/kul/dijleserver/controller/ApiController.java
+++ src/main/java/be/kul/dijleserver/controller/ApiController.java
@@ -1,9 +1,12 @@
 package be.kul.dijleserver.controller;
 
+import be.kul.dijleserver.domain.Pod;
 import be.kul.dijleserver.dto.ReadingDTO;
+import be.kul.dijleserver.repository.PodRepository;
 import be.kul.dijleserver.repository.ReadingRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,16 +18,17 @@ import static java.util.stream.Collectors.toList;
 @RequestMapping("/api/v1")
 public class ApiController {
 
-    private ReadingRepository readingRepository;
+    private PodRepository podRepository;
 
-    public ApiController(ReadingRepository readingRepository) {
-        this.readingRepository = readingRepository;
+    public ApiController(ReadingRepository readingRepository, PodRepository podRepository) {
+        this.podRepository = podRepository;
     }
 
-    @GetMapping("/readings")
+    @GetMapping("/pod/{pod}/readings")
     @ResponseBody
-    public List<ReadingDTO> getAllReadings() {
-        return readingRepository.findAll()
+    public List<ReadingDTO> getAllReadings(@PathVariable("pod") String podName) {
+        final Pod pod = podRepository.findByName(podName);
+        return pod.getReadings()
                 .stream()
                 .map(ReadingDTO::of)
                 .collect(toList());
--- src/main/java/be/kul/dijleserver/controller/WebController.java
+++ src/main/java/be/kul/dijleserver/controller/WebController.java
@@ -1,22 +1,45 @@
 package be.kul.dijleserver.controller;
 
+import be.kul.dijleserver.dto.PodDTO;
+import be.kul.dijleserver.repository.PodRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/")
 public class WebController {
+
+    private PodRepository podRepository;
+
+    public WebController(PodRepository podRepository) {
+        this.podRepository = podRepository;
+    }
 
     @GetMapping
     public String homepage() {
         return "/homepage";
     }
 
+    @GetMapping("/pod/{pod}")
+    public String homepage(@PathVariable("pod") String pod, Model model) {
+
+        model.addAttribute("pod", pod);
+
+        return "/pod";
+    }
+
+    @ModelAttribute("pods")
+    public List<PodDTO> pods() {
+        return podRepository.findAll()
+                .stream()
+                .map(PodDTO::of)
+                .sorted( (a, b) -> a.getName().compareToIgnoreCase(b.getName()))
+                .collect(toList());
+    }
+
 }
-
-
-
-
-
-
