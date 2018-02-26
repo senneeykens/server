7ca4f3ba8b14d66d824dd21785f3fe526090b015
Koen Dehaen <koen.dehaen@roots.be>
2018-02-26 20:46:52 +0100

    correct gprmc conversion to gps cooordinates

--- src/main/java/be/kul/dijleserver/dto/push/ReadingDTO.java
+++ src/main/java/be/kul/dijleserver/dto/push/ReadingDTO.java
@@ -35,20 +35,31 @@ public class ReadingDTO extends AbstractReadingDTO {
         reading.setDisolvedOxygen(this.getDisolvedOxygen());
         reading.setFlowRate(this.getFlowRate());
 
+
+        // see https://www.experts-exchange.com/questions/22112629/Converting-NMEA-sentence-Latitude-and-Longitude-to-Decimal-Degrees.html
+        // $GPRMC,204432.00,A,5111.59428,N,00432.82947,E,4.773,,250218,,,D*7E
         if (StringUtils.hasText(this.getGps())) {
             final String[] split = this.getGps().split(",");
-            reading.setLatitude(numberMagic (split[3]));
-            reading.setLongtitude(numberMagic (split[5]));
+
+            String ddd = split[3].substring(0, 2);
+            String mmm = split[3].substring(2);
+
+            reading.setLatitude(numberMagic(ddd, mmm));
+
+            ddd = split[5].substring(0, 3);
+            mmm = split[5].substring(3);
+
+            reading.setLongtitude(numberMagic(ddd, mmm));
 
         }
         return reading;
     }
 
-    private double numberMagic ( String number ) {
-        return BigDecimal.valueOf(
-                Double.valueOf(number))
-                    .divide(BigDecimal.valueOf(100), 6, RoundingMode.HALF_UP.ordinal()
-        ).doubleValue();
+    private double numberMagic(String ddd, String mmm) {
+        return Double.valueOf(ddd) +
+                BigDecimal.valueOf(
+                        Double.valueOf(mmm)).divide(BigDecimal.valueOf(60), 5, RoundingMode.HALF_UP.ordinal()
+                ).doubleValue();
     }
 
 }
--- src/test/java/be/kul/dijleserver/dto/push/ReadingDTOTest.java
+++ src/test/java/be/kul/dijleserver/dto/push/ReadingDTOTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.assertEquals;
 public class ReadingDTOTest {
 
     @Test
-    public void gpggaConversion() {
+    public void gpsConversion() {
 
         final ReadingDTO readingDTO = new ReadingDTO();
         readingDTO.setGps("$GPRMC,204432.00,A,5111.59428,N,00432.82947,E,4.773,,250218,,,D*7E");
 
-        assertEquals ( 51.115943, readingDTO.toReading().getLatitude(), 0.0 );
-        assertEquals ( 04.328295, readingDTO.toReading().getLongtitude(), 0.0 );
+        assertEquals ( 51.19324, readingDTO.toReading().getLatitude(), 0.0 );
+        assertEquals ( 4.54716, readingDTO.toReading().getLongtitude(), 0.0 );
 
     }
 
